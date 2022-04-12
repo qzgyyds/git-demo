a279e918dc6defd04dfe3fa77ee0162c873e1658
qzghl <qzghll@yeah.net>
2022-04-12 15:36:06 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yhm
+ * @create 2022-04-12 15:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test1");
+    }
+}
